b516a5db096033653b824a178ffeaac5f209a1fc
Ahmad <Faisal.Ahmad@vistracorp.com>
2025-09-25 13:32:33 -0500

    docker-initfile added

--- docker-init.sh
+++ docker-init.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-
 # Docker initialization script for texas-dps-scheduler
 
-# Check if HEADLESS environment variable is set to false
+# --- NEW PART ---
+# Create config.yml from Fly secret if present
+if [ -n "$CONFIG_YML_B64" ]; then
+  echo "$CONFIG_YML_B64" | base64 -d > ./config.yml
+fi
+# --- END NEW PART ---
+
+# Normal start logic
 if [ "$HEADLESS" = "false" ]; then
     echo "Starting with xvfb for non-headless mode..."
-    # Start xvfb with optimal settings for captcha avoidance
     xvfb-run -a -s '-screen 0 1920x1080x24 -ac -nolisten tcp -dpi 96' node index.js
 else
     echo "Starting in headless mode..."
